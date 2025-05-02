14603a2ba69882ff01e2b035ab676e9946913339
S Kazmi <alikazmi@yahoo.com>
2025-05-02 15:43:49 +0100

    first commit

---
+++ src/main/java/com/solnetev/phoenix/ocpp/jaxb/RequestType.java
@@ -0,0 +1,11 @@
+package com.solnetev.phoenix.ocpp.jaxb;
+
+
+/**
+ * To be implemented by all OCPP requests
+ *
+ * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
+ * @since 12.03.2015
+ */
+public interface RequestType {
+}
---
+++ src/main/java/com/solnetev/phoenix/ocpp/jaxb/ResponseType.java
@@ -0,0 +1,12 @@
+package com.solnetev.phoenix.ocpp.jaxb;
+
+
+
+/**
+ * To be implemented by all OCPP responses
+ *
+ * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
+ * @since 12.03.2015
+ */
+public interface ResponseType {
+}
